b5d2b701dd0008d888c6eba95861fb19f74ac2bd
vladimir <myweb11@mail.ru>
2023-02-13 22:31:13 +0300

    кастомные флеш уведомления

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -75,9 +75,13 @@ class AuthController extends Controller
             $request->only('email')
         );
 
-        return $status === Password::RESET_LINK_SENT
-            ? back()->with(['message' => __($status)])
-            : back()->withErrors(['email' => __($status)]);
+        if ($status === Password::RESET_LINK_SENT) {
+            flash()->info(__($status));
+
+            return back()->with(['message' => __($status)]);
+        }
+
+        return back()->withErrors(['email' => __($status)]);
     }
 
     public function passwordReset(string $token)
@@ -100,9 +104,13 @@ class AuthController extends Controller
             }
         );
 
-        return $status === Password::PASSWORD_RESET
-            ? redirect()->route('login')->with('message', __($status))
-            : back()->withErrors(['email' => [__($status)]]);
+        if ($status === Password::PASSWORD_RESET) {
+            flash()->info(__($status));
+
+            return redirect()->route('login');
+        }
+
+        return back()->withErrors(['email' => [__($status)]]);
     }
 
     public function logout(): RedirectResponse
